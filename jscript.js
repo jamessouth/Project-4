f1ef653ec2edb0dd05b0649175ba5b1c7f3cabfc
jamessouth <jamesdanielsouth@gmail.com>
2017-03-25 22:34:01 -0500

    uploads

--- jscript.js
+++ jscript.js
@@ -27,38 +27,50 @@ $searchbox.on('input', function(){
 			
 		}
 	}
+	
 	console.log('\n');
-
-});
-
-$('a').on('click', function(){
-	console.log('ddddddddddddddddddd');
+	
+	$('a').filter(function(){
+		return $(this).css('display') !== 'none';
+		}).featherlightGallery();
+	
+	
+	
+	
+	
+	
+	
+	
 	
 });
 
 
-
-window.addEventListener('pageshow', function(event) {
-    console.log('pageshow:');
-    console.log(event);
+// window.addEventListener('pageshow', function(event) {
+    // console.log('pageshow:');
+    // console.log(event);
 	
-		for (var i = 0; i < paras.length; i++){
-		// console.log(paras[i].includes($searchbox.val()));
+		// for (var i = 0; i < paras.length; i++){
+		// // console.log(paras[i].includes($searchbox.val()));
 		
-		if (paras[i].includes($searchbox.val().toLowerCase()) === false){
-			$($paras[i]).parent('a').hide();
-			console.log(paras[i].includes($searchbox.val().toLowerCase()), paras[i]);
-		} else {
+		// if (paras[i].includes($searchbox.val().toLowerCase()) === false){
+			// $($paras[i]).parent('a').hide();
+			// console.log(paras[i].includes($searchbox.val().toLowerCase()), paras[i]);
+		// } else {
 			
-			$($paras[i]).parent('a').show();
-			console.log(paras[i].includes($searchbox.val().toLowerCase()), paras[i]);
+			// $($paras[i]).parent('a').show();
+			// console.log(paras[i].includes($searchbox.val().toLowerCase()), paras[i]);
 			
-		}
-	}
-	console.log('\n');
+		// }
+	// }
+	// console.log('\n');
 	
 	
-});
+// });
+
+
+
+
+
 
 
 
