6e0831765f121f37d2baa294081fe1bd8c683f0e
jamessouth <jamesdanielsouth@gmail.com>
2017-04-02 18:25:32 -0500

    uploads

--- jscript.js
+++ jscript.js
@@ -1,48 +1,22 @@
 var $searchbox = $('.search');
-
 var $paras = $('p');
 var paras = [];
 
-
-
 for (var i = 0; i < $paras.length; i++){
 		paras.push($paras[i].innerHTML.toLowerCase());
-		
 	}
-	
-
-
 
 $searchbox.on('input', function(){
-		for (var i = 0; i < paras.length; i++){
-		// console.log(paras[i].includes($searchbox.val()));
-		
+	for (var i = 0; i < paras.length; i++){
 		if (paras[i].includes($searchbox.val().toLowerCase()) === false){
-			
 			$($paras[i]).parent('a').hide();
-			
-			console.log(paras[i].includes($searchbox.val().toLowerCase()), paras[i]);
 		} else {
-			
 			$($paras[i]).parent('a').show();
-			
-			console.log(paras[i].includes($searchbox.val().toLowerCase()), paras[i]);
-			
 		}
 	}
-	
-	console.log('\n');
-	
-	
-	
 });
 
-
-
-
-
 $('#pics').featherlightGallery({
-	
 	filter: 'a:visible',
 	previousIcon: '&#60;',
 	nextIcon: '&#62;',
